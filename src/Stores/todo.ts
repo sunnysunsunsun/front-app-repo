3abaea35650ead6c4ee75376235a55ae58a1ce15
unknown <jisoenlee86@gamil.com>
2025-07-10 17:54:42 +0900

    710 1754

--- src/Stores/todo.ts
+++ src/Stores/todo.ts
@@ -11,20 +11,27 @@ interface Todo {
   createdAt: string // 할 일 생성일
   updatedAt: string // 할 일 수정일
 }
+
 export const useTodoStore = create(
-  combine({ todos: [] as Todos }, () => {
-    return {
-      fetchTodos: async () => {
-        const { data } = axios({
-          url: 'curl https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
-          method: 'GET',
-          headers: {
-            'content-type': 'application/json',
-            apikey: import.meta.env.VITE_APIKEY,
-            username: import.meta.env.VITE_USERNAME
-          }
-        })
+  combine(
+    {
+      todos: [] as Todos
+    },
+    () => {
+      return {
+        fetchTodos: async () => {
+          const { data } = await axios({
+            url: 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos',
+            method: 'GET',
+            headers: {
+              'content-type': 'application/json',
+              apikey: import.meta.env.VITE_APIKEY,
+              username: import.meta.env.VITE_USERNAME
+            }
+          })
+          console.log(data)
+        }
       }
     }
-  })
+  )
 )
---
+++ src/types/vite-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types = "vite/client" />
+interface ImportMetaEnv {
+  VITE_APIKEY: string
+  VITE_USERNAME: string
+}
